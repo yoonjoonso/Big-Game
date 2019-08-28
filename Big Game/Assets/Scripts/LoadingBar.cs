7f0fe82d7595de9df2e2394aa2cc7ded8747be6d
Yoon <yoon.so@humanmode.com>
2019-08-28 09:44:24 -0500

    Fmod integration

--- Big Game/Assets/Scripts/LoadingBar.cs
+++ Big Game/Assets/Scripts/LoadingBar.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LoadingBar : MonoBehaviour
 {
+    public float completeAmount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,7 @@ public class LoadingBar : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         print("triggered by: " + other);
-        if(other.CompareTag("Data"))
+        if (other.CompareTag("Data"))
         {
             Destroy(other.gameObject);
         }
