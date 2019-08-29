aaf460c1cf28839a60a7d2c00396dd14f7c0704c
Yoon <yoon.so@humanmode.com>
2019-08-28 20:39:54 -0500

    fmod integration

---
+++ Big Game/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using FMODUnity;
+
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public DataFactory dataFactory;
+
+    public LoadingBar loadingBar;
+
+    public StudioEventEmitter eventEmitter;
+
+    private void Update()
+    {
+        eventEmitter.SetParameter("Intensity", dataFactory.enemyIntensity);
+        eventEmitter.SetParameter("Progress", loadingBar.completeAmount);
+    }
+}
